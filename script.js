d325bba9e44eac21d2c4e8786023f1401b4035b3
developer <prabesh065@gmail.com>
2025-01-29 03:07:13 +0545

    few chages

--- script.js
+++ script.js
@@ -3,10 +3,41 @@ const noBtn = document.querySelector(".no-btn");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 
+// List of quotes
+const quotes = [
+  "You are my today and all of my tomorrows.",
+  "Loving you is the best decision I’ve ever made.",
+  "You are my sunshine on the darkest days.",
+  "With you, I’ve found my forever.",
+  "You are the reason I believe in love.",
+  "My heart is and always will be yours.",
+  "You are my favorite place to be.",
+  "Every moment with you feels like a dream come true.",
+  "You are my always and forever.",
+  "I love you more than words could ever say."
+];
+
+// List of GIFs
+const gifs = [
+  "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGRpdnowemg4NWNoYzY2ZTlzc2tzNzR1NHpsbzgxcG5zY3VhMmdrbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jv2NrCzvwqPh6/giphy.gif", // Cute couple
+  "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMm1xM2d5aWo4bGVsZXJkZWh2YXFpeWI1emUyZ3V6dGIyNnQzOG10eSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/NxC8VtyxqhMtpLoEEN/giphy.gif", // Blowing kisses
+  "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExeXZpb2Z3eG05ejgzZnlzZWQ0YjRtazVpdnlrbnQwM2N6aWdoeTc3biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/P3CZolxd8DeRy4g4fM/giphy.gif", // Hugging
+  "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMGNob3dsdGViYzAyeWM3bTZiaXU3ejU4aWloNjM3bnEyeTQybGpkOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/PGp3hlI74uVxK/giphy.gif", // Dancing together
+  "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWRucWhyZG85cnhjdzlqNjNoYm5zNnprMTA0NjJpNWx0ZDVnYXpibCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ZBKEV8xZElWpCKDTBo/giphy.gif", // Heart-shaped fireworks
+  "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGVneDVzdHYxOXdkamZzY2QxeTk4OHh4ZHQyd3Q0d3dnaHE1M2VibiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/rkSu72ptAZseQ/giphy.gif", // Romantic sunset
+  "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNnhtenFtZG5jeHNsOWIxanN1azBmOXVibXFxZXI1NHhwNWhqYWpkZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/PmQvERtQkKuDLgl12K/giphy.gif", // Love explosion
+  "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmZjenh6OWNkdGQyYnBsdzk2MHYxaTJhNWRibjNxY2JqbTBwb2NrZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/IwGL1W9SISNLuIPJDM/giphy.gif", // Heart eyes
+  "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbnZuZjg1bWE4Ynl4cXVnODM3djA0ZmZkMTlwdmQzYzVzaDAxcmJxZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/r4UG27LD5Ee5y/giphy.gif" // Cute love
+];
+
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Your love is the melody that makes my heart sing every single day.";
-  gif.src = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExYnc1ejQzbnhmMHVkYXV3bDU5bDJjejdvZjVub3B0MnN4N2VqN2dkOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MeIucAjPKoA120R7sN/giphy.gif";
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  const randomGif = gifs[randomIndex]; // Use the same index for consistency
+
+  question.innerHTML = randomQuote;
+  gif.src = randomGif;
 });
 
 // Make the No button move randomly on hover
